e2b649e308cf33964a623000cc0cbe687e6f3538
Obinna Emmanuel <ekwealorobinna112@gmail.com>
2021-02-04 23:22:59 +0100

    initial commit

---
+++ Desktop/Projects/netflix-build-youtube/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBli6J5V_TwoFa8khwdOkolU8xQKnptX1M",
+    authDomain: "netflix-clone-6dc45.firebaseapp.com",
+    projectId: "netflix-clone-6dc45",
+    storageBucket: "netflix-clone-6dc45.appspot.com",
+    messagingSenderId: "485749207168",
+    appId: "1:485749207168:web:af759cb1d87faa3ad17665"
+  };
